961639df438d204558d2b64e54e72ed384fb3ea4
Прушак Евгений Владимирович <D:\poshta>
2020-04-15 12:53:01 +0300

    refacotor: format code

--- snake.js
+++ snake.js
@@ -1,4 +1,4 @@
-function drawRect (param) {
+function drawRect(param) {
     context.beginPath();
     context.rect(param.x, param.y, param.width, param.height);
     context.fillStyle = param.fillColor;
@@ -12,10 +12,10 @@ function clearCanvas() {
 
 function createGameMap(columns, rows) {
     const map = [];
-// массив в массиве = матрица 
+    // массив в массиве = матрица 
     for (let x = 0; x < columns; x++) {
         const row = [];
-        
+
         for (let y = 0; y < rows; y++) {
             row.push({
                 x: x,
@@ -23,11 +23,10 @@ function createGameMap(columns, rows) {
                 snake: false,
                 food: false,
             });
-            
+
         }
 
         map.push(row);
-        
     }
 
     return map;
@@ -43,16 +42,16 @@ function getRandomFreeCell(map) {
 
         freeCells.push(cell);
     }
-    
+
     const index = Math.floor(Math.random() * freeCells.length);
-    return freeCells[index];    
+    return freeCells[index];
 }
 
 function drawGameMap(map) {
     for (const cell of map.flat()) {
         const param = {
-            x: GAME_PADING +  cell.x * (CELL_SIZE + CELL_MARGIN),
-            y: GAME_PADING +  cell.y * (CELL_SIZE + CELL_MARGIN),
+            x: GAME_PADING + cell.x * (CELL_SIZE + CELL_MARGIN),
+            y: GAME_PADING + cell.y * (CELL_SIZE + CELL_MARGIN),
             width: CELL_SIZE,
             height: CELL_SIZE,
             fillColor: FREE_COLOR,
@@ -67,11 +66,11 @@ function drawGameMap(map) {
         }
 
 
-        drawRect(param);     
+        drawRect(param);
     }
 }
 
-function  getCell (x, y) {
+function getCell(x, y) {
     if (x < 0) {
         x += COLUMNS;
     }
@@ -88,7 +87,7 @@ function  getCell (x, y) {
         y -= ROWS;
     }
 
-    for (const cell  of map.flat()) {
+    for (const cell of map.flat()) {
         if (cell.x === x && cell.y === y) {
             return cell;
         }
@@ -96,28 +95,62 @@ function  getCell (x, y) {
 }
 
 function moveSnake() {
- if (snakeDirect === 'left') {
-    snake[0] = getCell(snake[0].x - 1, snake[0].y);
- }
+    for (let i = snake.length - 1; i > 0; i--) {
+        snake[i] = snake[i - 1];
 
- else if (snakeDirect === 'right') {
-    snake[0] = getCell(snake[0].x + 1, snake[0].y);
- }
+    }
+    if (snakeDirect === 'left') {
+        snake[0] = getCell(snake[0].x - 1, snake[0].y);
+    } else if (snakeDirect === 'right') {
+        snake[0] = getCell(snake[0].x + 1, snake[0].y);
+    } else if (snakeDirect === 'down') {
+        snake[0] = getCell(snake[0].x, snake[0].y + 1);
+    } else if (snakeDirect === 'up') {
+        snake[0] = getCell(snake[0].x, snake[0].y - 1);
+    }
 
- else if (snakeDirect === 'down') {
-    snake[0] = getCell(snake[0].x, snake[0].y + 1);  
- }
+    for (const cell of map.flat()) {
+        cell.snake = false;
+    }
 
- else if (snakeDirect === 'up') {
-    snake[0] = getCell(snake[0].x , snake[0].y - 1);
+    for (const cell of snake) {
+        cell.snake = true;
+    }
 }
 
-for (const cell  of map.flat()) {
-    cell.snake = false;
+function showState() {
+    context.font = "30px Arial";
+    context.fillStyle = "black";
+    context.textAlign = "left";
+    context.fillText(`Cooldown: ${cooldown}`, 10, 30);
+    context.fillText(`Очки: ${snake.length * 5}`, 10, 50);
 }
 
-for (const cell of snake) {
+function drawPause() {
+    context.beginPath();
+    context.rect(0, 0, canvas.width, canvas.height);
+    context.fillStyle = 'rgba(255, 255, 255, 0.7)';
+    context.fill();
+    context.font = "30px Arial";
+    context.fillStyle = "black";
+    context.textAlign = "center";
+    context.fillText(`Ваш счет: ${cooldown}`, canvas.width / 2, canvas.height / 2);
+    context.fillText("Нажмите enter для продолжения", canvas.width / 2, canvas.height / 2 + 58);
+}
+
+function init() {
+    map = createGameMap(COLUMNS, ROWS);
+
+    
+    const cell = getRandomFreeCell(map);
+
+    snake = [cell];
+
     cell.snake = true;
-}
-}
 
+    snakeDirect = 'up';
+    nextSnakeDirect = 'up';
+    play = true;
+    cooldown = START_COOLDOWN;
+    getRandomFreeCell(map).food = true;
+}
